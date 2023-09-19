57c0f4b2dbfca5e5a4644881e1ef7c330e807b6d
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-09-19 19:25:25 -0400

    created server

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const pg = require('pg');
 const client = new pg.Client('postgres://localhost/ice_cream_shop_db')
+const express = require('express');
+const app = express();
+
+app.get('/api/ice_cream' , async (req, res, next) => {
+    try{ 
+        const SQL = `
+        SELECT * from ice_cream
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows)
+    }
+    catch(ex){
+        next(ex);
+    }
+})
 
 const setup = async () => {
     await client.connect()
@@ -11,12 +26,15 @@ const setup = async () => {
         name VARCHAR(20)
 
     );
-    INSERT INTO ice_cream (name) VALUES ('vanilla')
-    INSERT INTO ice_cream (name) VALUES ('chocolate')
-    INSERT INTO ice_cream (name) VALUES ('strawberry')
-    INSERT INTO ice_cream (name) VALUES ('cookie dough')
+
+    INSERT INTO ice_cream (name) values ('vanilla');
+    INSERT INTO ice_cream (name) values ('chocolate');
+    INSERT INTO ice_cream (name) values ('strawberry');
+    
     `;
     await client.query(SQL);
     console.log("tables created")
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on port ${port}`))
 };
 setup();
